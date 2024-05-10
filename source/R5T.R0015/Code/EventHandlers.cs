2d8ef07cd969848accf5273330f00763023a2be3
davidcoats <david@getsafetycone.com>
2024-05-10 00:07:20 -0700

    Interim changes.

---
+++ source/R5T.R0015/Code/EventHandlers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Components;
+
+using R5T.T0142;
+using R5T.T0154;
+
+
+namespace R5T.R0015
+{
+    /// <summary>
+    /// Host type for custom event <see cref="EventHandlerAttribute"/> attributes.
+    /// </summary>
+    /// <remarks>
+    /// For helpful documentation, see the documentation type: <see cref="Y0007.Documentation.For_EventHandlers"/>.
+    /// </remarks>
+    [EventHandler("ontransitionend", typeof(EventArgs), enableStopPropagation: true, enablePreventDefault: false)]
+    [EventHandler("onanimationend", typeof(EventArgs), enableStopPropagation: true, enablePreventDefault: false)]
+    [RazorComponentMarker] // Also put this type in the Razor components list.
+    [UtilityTypeMarker]
+    public static class EventHandlers
+    {
+    }
+}
